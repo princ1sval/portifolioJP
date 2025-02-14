e3a3abfd49cef8097864ca11b61df75bf1c48c6a
João Pedro <jp.princisval@gmail.com>
2025-02-14 20:27:17 -0300

    PrimeiroCommit

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuIcon = document.querySelector(".menuIcon");
-    const menu = document.querySelector(".menu");
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.querySelector('.menuIcon');
+    const menu = document.querySelector('.menu');
 
-    menuIcon.addEventListener("click", function() {
-        // Alterna a visibilidade do menu
-        if (menu.style.display === "block") {
-            menu.style.display = "none"; // Fecha o menu
-        } else {
-            menu.style.display = "block"; // Abre o menu
-        }
+    menuIcon.addEventListener('click', function() {
+        menu.classList.toggle('active');
     });
 });
